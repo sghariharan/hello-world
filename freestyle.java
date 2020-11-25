61929c550397c94f05403f33c507c24dfdc6e1a9
Hariharan <hariharanmsg@gmail.com>
2020-11-25 08:44:08 +0000

    freestyle1

---
+++ freestyle.java
@@ -0,0 +1,5 @@
+public class freestyle{
+    public static void name(String[] args) {
+       System.out.println("Hello World to Java");
+}
+}
--- hello -world.java
+++
@@ -1,7 +0,0 @@
-public class Helloworld{
-   Run|Debug
-   public static void main(String[] args){
-      System.out.println("Hello World to Java")
-
-}
-}
